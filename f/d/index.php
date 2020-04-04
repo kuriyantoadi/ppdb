6dddba4553cc666b519b9d30a42518177087bf62
kuriyantoadi <melatikuning212@gmail.com>
2020-04-05 02:53:51 +0700

    push 2

--- f/d/index.php
+++ f/d/index.php
@@ -23,8 +23,19 @@
   <center><h2>Form Pendaftaran Calon Peserta Didik</h2></center>
   <center><h2>SMKN 1 Kragilan</h2></center>
 
+  <br><br><br>
 
-  <br><a href="../logout.php" type="button" class="btn btn-danger">Logout</a><br><br>
+  <div class="form-group">
+    <div class="col-sm-7">
+      <a href="../logout.php" type="button" class="btn btn-danger">Logout</a>
+    </div>
+    <label class="control-label col-sm-2" for="email">Cari Peserta Calon Peserta Didik :</label>
+    <div class="col-sm-3">
+      <input type='text' class="form-control"  id='input' onkeyup='searchTable()'>
+    </div>
+
+
+  </div>
 
   <table class="table table-bordered table-hover" id="domainsTable">
   <thead>
@@ -47,14 +58,15 @@
       <td>TKJ</td>
       <td>Asal Sekolah</td>
       <td>
+        <center>Acc</center>
+      </td>
+      <td>
         <center>
           <a href="#" class="btn btn-primary btn-sm">
             <span class="glyphicon glyphicon-pencil"></span>
           </a>
       </td>
-      <td>
-        <center>Acc</center>
-      </td>
+
     </tr>
     <tr>
       <td><center>1</td>
@@ -63,14 +75,15 @@
       <td>RPL</td>
       <td>Asal Sekolah</td>
       <td>
+        <center>Acc</center>
+      </td>
+      <td>
         <center>
           <a href="#" class="btn btn-primary btn-sm">
             <span class="glyphicon glyphicon-pencil"></span>
           </a>
       </td>
-      <td>
-        <center>Acc</center>
-      </td>
+
     </tr>
   </tbody>
 </table>
@@ -81,6 +94,36 @@
             $("#domainsTable").tablesorter({sortList: [[3,1],[2,0]]});
         }
     );
+
+    function searchTable() {
+        var input;
+        var saring;
+        var status;
+        var tbody;
+        var tr;
+        var td;
+        var i;
+        var j;
+        input = document.getElementById("input");
+        saring = input.value.toUpperCase();
+        tbody = document.getElementsByTagName("tbody")[0];;
+        tr = tbody.getElementsByTagName("tr");
+        for (i = 0; i < tr.length; i++) {
+            td = tr[i].getElementsByTagName("td");
+            for (j = 0; j < td.length; j++) {
+                if (td[j].innerHTML.toUpperCase().indexOf(saring) > -1) {
+                    status = true;
+                }
+            }
+            if (status) {
+                tr[i].style.display = "";
+                status = false;
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+
     </script>
 </body>
 </html>
