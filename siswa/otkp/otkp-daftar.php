c9bb2da4f15b226b4ab93c38bf49866f99fc2ca2
kuriyantoadi <melatikuning212@gmail.com>
2020-05-14 03:15:32 +0700

    update 14 mei 2020, sesi 2

--- siswa/otkp/otkp-daftar.php
+++ siswa/otkp/otkp-daftar.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <title>Calon Siswa sudah Mendaftar</title>
   <meta charset="utf-8">
@@ -7,78 +8,80 @@
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-  <script type="text/javascript" src="../js/jquery-latest.js"></script>
-  <script type="text/javascript" src="../js/jquery.tablesorter.min.js"></script>
+  <script type="text/javascript" src="../../js/jquery-latest.js"></script>
+  <script type="text/javascript" src="../../js/jquery.tablesorter.min.js"></script>
 </head>
+
 <body>
 
 
 
-<div class="container">
-  <div class="row">
-    <div class="col-md-3">
-      <center><img style="margin-top: 25px;" src="../../images/logo-banten.png" />
-    </div>
-    <div class="col-md-6">
-      <center>
-        <h2 style="margin-top:  25px;"><b>SMK Negeri 1 Kragilan</b></h2>
-      </center>
-      <center>
-        <h4><b>Daftar Calon Peserta Didik yang sudah Mendaftar</b></h4>
-      </center>
-      <center>
-        <h4><b>Calon Peserta Didik Baru</b></h4>
-      </center>
-      <center>
-        <h5><b>Tahun Pelajaran 2020/2021</b></h4>
-      </center>
-      <center>
-        <h4><b>Program Studi Otomatisasi Tata Kelola Perkantoran</b></h4>
-      </center><br>
-      <!-- font ganti jenis -->
-    </div>
-    <div class="col-md-3">
-      <center><img style="margin-bottom:  80px; margin-top:  25px;" class="img-fluid" alt="Bootstrap Image Preview" src="../../images/logo-smkn1.png" />
-    </div>
-  </div>
-
-  <div class="form-group">
-    <label class="control-label col-sm-2" for="email">Cari Peserta Calon Peserta Didik :</label>
-    <div class="col-sm-3">
-      <input type='text' class="form-control" id='input' onkeyup='searchTable()'>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-3">
+        <center><img style="margin-top: 25px;" src="../../images/logo-banten.png" />
+      </div>
+      <div class="col-md-6">
+        <center>
+          <h2 style="margin-top:  25px;"><b>SMK Negeri 1 Kragilan</b></h2>
+        </center>
+        <center>
+          <h4><b>Daftar Calon Peserta Didik yang sudah Mendaftar</b></h4>
+        </center>
+        <center>
+          <h4><b>Calon Peserta Didik Baru</b></h4>
+        </center>
+        <center>
+          <h5><b>Tahun Pelajaran 2020/2021</b></h4>
+        </center>
+        <center>
+          <h4><b>Program Studi Otomatisasi Tata Kelola Perkantoran</b></h4>
+        </center><br>
+        <!-- font ganti jenis -->
+      </div>
+      <div class="col-md-3">
+        <center><img style="margin-bottom:  80px; margin-top:  25px;" class="img-fluid" alt="Bootstrap Image Preview" src="../../images/logo-smkn1.png" />
+      </div>
     </div>
 
+    <div class="form-group">
+      <label class="control-label col-sm-2" for="email">Cari Peserta Calon Peserta Didik :</label>
+      <div class="col-sm-3">
+        <input type='text' class="form-control" id='input' onkeyup='searchTable()'>
+      </div>
 
-  </div>
 
-  <table class="table table-bordered table-hover" id="domainsTable">
-    <thead>
-      <tr>
-        <th>
-          <center>No
-        </th>
-        <th>
-          <center>Nomor Pendaftaran
-        </th>
-        <th>
-          <center>Tanggal Pendaftaran
-        </th>
-        <th>
-          <center>NISN Siswa
-        </th>
-        <th>
-          <center>Nama Siswa
-        </th>
-        <th>
-          <center>Kompetensi Keahlian
-        </th>
-        <th>
-          <center>Asal Sekolah
-        </th>
-      </tr>
-    </thead>
-    <tbody>
-    <?php
+    </div>
+
+    <table class="table table-bordered table-hover" id="domainsTable">
+      <thead>
+        <tr>
+          <th>
+            <center>No
+          </th>
+          <th>
+            <center>Nomor Pendaftaran
+          </th>
+          <th>
+            <center>Tanggal Pendaftaran
+          </th>
+          <th>
+            <center>NISN Siswa
+          </th>
+          <th>
+            <center>Nama Siswa
+          </th>
+          <th>
+            <center>Asal Sekolah
+          </th>
+          <!-- Data di buka bos :) -->
+          <!-- <th>
+            <center>Lolos Seleksi
+          </th> -->
+        </tr>
+      </thead>
+      <tbody>
+        <?php
       include '../../koneksi.php';
       $halperpage = 50;
       $page = isset($_GET["halaman"]) ? (int)$_GET["halaman"] : 1;
@@ -96,67 +99,84 @@
       while ($d = mysqli_fetch_array($data)) {
           ?>
 
-    <tr>
-      <td><center><?php echo $no++ ?></td>
-      <td><center><?php echo $d['no_p']; ?></td>
-      <td><center><?php echo $d['tgl_pendaftaran']; ?></td>
-      <td><center><?php echo $d['nisn']; ?></td>
-      <td><center><?php echo $d['nama_siswa']; ?></td>
-      <td><center><?php echo $d['kompetensi_keahlian']; ?></td>
-      <td><center><?php echo $d['asal_sekolah']; ?></td>
-    </tr>
+        <tr>
+          <td>
+            <center><?php echo $no++ ?>
+          </td>
+          <td>
+            <center><?php echo $d['no_p']; ?>
+          </td>
+          <td>
+            <center><?php echo $d['tgl_pendaftaran']; ?>
+          </td>
+          <td>
+            <center><?php echo $d['nisn']; ?>
+          </td>
+          <td>
+            <center><?php echo $d['nama_siswa']; ?>
+          </td>
+          <td>
+            <center><?php echo $d['asal_sekolah']; ?>
+          </td>
+          <!-- Data di buka bos :) -->
+          <!-- <td>
+            <center><?php echo $d['kondisi']; ?>
+          </td> -->
+        </tr>
 
-    <?php
+        <?php
       } ?>
-  </tbody>
-</table>
-<div>
-  <?php for ($i=1; $i<=$pages ; $i++) {
+      </tbody>
+    </table>
+    <div>
+      <?php for ($i=1; $i<=$pages ; $i++) {
           ?>
-  <a class="btn btn-info btn-md" href="?halaman=<?php echo $i; ?>"><?php echo $i; ?></a>
-  <?php
+      <a class="btn btn-info btn-md" href="?halaman=<?php echo $i; ?>"><?php echo $i; ?></a>
+      <?php
       } // database
 
   ?>
-</div>
-</div>
-<script>
-    $(document).ready(function()
-        {
-            $("#domainsTable").tablesorter({sortList: [[3,1],[2,0]]});
-        }
-    );
+    </div>
+  </div>
+  <script>
+    $(document).ready(function() {
+      $("#domainsTable").tablesorter({
+        sortList: [
+          [3, 1],
+          [2, 0]
+        ]
+      });
+    });
 
     function searchTable() {
-        var input;
-        var saring;
-        var status;
-        var tbody;
-        var tr;
-        var td;
-        var i;
-        var j;
-        input = document.getElementById("input");
-        saring = input.value.toUpperCase();
-        tbody = document.getElementsByTagName("tbody")[0];;
-        tr = tbody.getElementsByTagName("tr");
-        for (i = 0; i < tr.length; i++) {
-            td = tr[i].getElementsByTagName("td");
-            for (j = 0; j < td.length; j++) {
-                if (td[j].innerHTML.toUpperCase().indexOf(saring) > -1) {
-                    status = true;
-                }
-            }
-            if (status) {
-                tr[i].style.display = "";
-                status = false;
-            } else {
-                tr[i].style.display = "none";
-            }
+      var input;
+      var saring;
+      var status;
+      var tbody;
+      var tr;
+      var td;
+      var i;
+      var j;
+      input = document.getElementById("input");
+      saring = input.value.toUpperCase();
+      tbody = document.getElementsByTagName("tbody")[0];;
+      tr = tbody.getElementsByTagName("tr");
+      for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td");
+        for (j = 0; j < td.length; j++) {
+          if (td[j].innerHTML.toUpperCase().indexOf(saring) > -1) {
+            status = true;
+          }
         }
+        if (status) {
+          tr[i].style.display = "";
+          status = false;
+        } else {
+          tr[i].style.display = "none";
+        }
+      }
     }
-
-
-    </script>
+  </script>
 </body>
+
 </html>
--- siswa/rpl/rpl-daftar.php
+++ siswa/rpl/rpl-daftar.php
@@ -8,8 +8,8 @@
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-  <script type="text/javascript" src="../0-js/jquery-latest.js"></script>
-  <script type="text/javascript" src="../0-js/jquery.tablesorter.min.js"></script>
+  <script type="text/javascript" src="../../js/jquery-latest.js"></script>
+  <script type="text/javascript" src="../../js/jquery.tablesorter.min.js"></script>
 </head>
 
 <body>
